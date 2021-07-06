30210c579cf1a684fa9e1cfb3f9313e971783a2b
Beatrizgomesv <82125152+Beatrizgomesv@users.noreply.github.com>
2021-07-05 21:04:33 -0300

    identação do código

--- src/projetoBanco/Conta.java
+++ src/projetoBanco/Conta.java
@@ -2,51 +2,47 @@ package projetoBanco;
 
 public class Conta {
 
-private int numero;
-private String titular;
-private double saldo;
-	
-public Conta(int numero, String titular) {
-	this.numero = numero;
-	this.titular = titular;
-}
+	private int numero;
+	private String titular;
+	private double saldo;
 
-public Conta (int numero, String titular, double depositoInicial) {
-	this.numero = numero;
-	this.titular = titular;
-	deposito(depositoInicial);
-}
+	public Conta(int numero, String titular) {
+		this.numero = numero;
+		this.titular = titular;
+	}
 
-public int getNumero() {
-	return numero;
-}
+	public Conta(int numero, String titular, double depositoInicial) {
+		this.numero = numero;
+		this.titular = titular;
+		deposito(depositoInicial);
+	}
 
-public String getTitular() {
-	return titular;
-}
+	public int getNumero() {
+		return numero;
+	}
 
-public void setTitular(String titular) {
-	this.titular = titular;
-}
+	public String getTitular() {
+		return titular;
+	}
 
-public double getSaldo() {
-	return saldo;
-}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
 
-public void deposito (double deposito) {
-	saldo += deposito;
-}
-public void saque (double deposito) {
-	saldo -= deposito + 5.0;
-}
+	public double getSaldo() {
+		return saldo;
+	}
 
-public String toString() {
-	return "\nConta Nº: " 
-			+numero
-			+"\nTitular: "
-			+ titular
-			+ "\nSaldo: $ "
-			+ String.format("%.2f",saldo);
-}
+	public void deposito(double deposito) {
+		saldo += deposito;
+	}
+
+	public void saque(double deposito) {
+		saldo -= deposito + 5.0;
+	}
+
+	public String toString() {
+		return "\nConta Nº: " + numero + "\nTitular: " + titular + "\nSaldo: $ " + String.format("%.2f", saldo);
+	}
 
 }
